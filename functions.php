71fb32e19ce2c23b1f138a76f86c3905e276faa3
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-22 14:41:07 +0200

    Add custom logos for website sections

--- functions.php
+++ functions.php
@@ -1,4 +1,23 @@
 <?php // Opening PHP tag - nothing should be before this, not even whitespace
+
+function get_logo_image_url() {
+    if ( is_home() ) {
+        return get_stylesheet_directory_uri() . '/img/logo-index.jpg';
+    }
+    elseif ( is_page ( 'categories' ) ) {
+        return get_stylesheet_directory_uri() . '/img/logo-categories.jpg';
+    }
+    elseif ( is_category ( 'lifestyle' ) ) {
+        return get_stylesheet_directory_uri() . '/img/logo-lifestyle.jpg';
+    }
+    elseif ( is_page ( 'devenir-ambassadeur' ) ) {
+        return get_stylesheet_directory_uri() . '/img/logo-devenir-ambassadeur.jpg';
+    }
+    else {
+        return get_stylesheet_directory_uri() . '/img/logo-default.jpg';
+    }
+}
+
 add_action( 'wp_enqueue_scripts', 'neptune_child_enqueue_styles' );
 
 function neptune_child_enqueue_styles() {
