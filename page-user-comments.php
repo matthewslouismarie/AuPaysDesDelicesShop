e0a580d3f718addd71ab0e7995d4d62ac56011f1
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-26 16:33:07 +0200

    Create user-comments page template

---
+++ page-user-comments.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: User Comments
+ * 
+ * Displays the comments of the currently logged-in user.
+ * 
+ * @package Au_Pays_Des_Delices
+ * @since 1.0.0
+ */
+
+get_header();
+
+get_footer();
