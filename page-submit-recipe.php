496f95aa678c6d81b535ab89fb82699cfdcd7841
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-28 18:58:01 +0200

    Fix call to unexisting function

--- page-submit-recipe.php
+++ page-submit-recipe.php
@@ -18,7 +18,7 @@ display_main_container_opening_tags();
 if ( ! is_user_logged_in() ) {
 	display_error_not_a_member();
 } elseif ( usp_is_successful_submission() ) {
-	usp_display_successful_post_submission();
+	usp_display_successful_submission();
 } else {
 	display_front_end_submit_recipe_form( $usp_options );
 }
