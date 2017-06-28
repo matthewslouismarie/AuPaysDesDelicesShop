09e24771b7dcb6ae2ed682bd8cdf93d8a46c3062
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-28 17:49:32 +0200

    Move USP into its own function

--- page-submit-recipe.php
+++ page-submit-recipe.php
@@ -6,6 +6,7 @@
 require_once( $functions . 'display-top-bar.php' );
 require_once( $functions . 'display-main-container-opening-tags.php' );
 require_once( $functions . 'display-main-container-closing-tags.php' );
+require_once( $functions . 'display-front-end-submit-recipe-form.php' );
 
 get_header();
 
@@ -14,9 +15,10 @@ display_top_bar( get_the_ID() );
 display_main_container_opening_tags();
 
 if ( ! is_user_logged_in() ) {
+	// TODO
 	echo "vous devez être connectés";
 } else {
-	require_once( $functions . 'display-front-end-submit-recipe-form.php' );
+	display_front_end_submit_recipe_form();
 }
 
 display_main_container_closing_tags();
