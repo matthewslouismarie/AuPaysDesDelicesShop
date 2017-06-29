bfd21d65728a2b2b26a0fc1f5c182ca2193b3ffb
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-06-29 15:38:29 +0200

    Fix obsolete imports in functions.php

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 
 // Constants
 define( 'FUNCTIONS_PATH' , get_stylesheet_directory() . '/functions/' );
-define( 'USP_CORE_CONSTANTS_AND_FUNCTIONS_PATH', get_stylesheet_directory() . '/user-submitted-posts/core-constants-and-functions.php' );
-define( 'USP_FORM_DISPLAYING_FUNCTIONS_PATH', get_stylesheet_directory() . '/user-submitted-posts/form-displaying-functions.php' );
-define( 'USP_FORM_PROCESSING_FUNCTIONS_PATH', get_stylesheet_directory() . '/user-submitted-posts/form-processing-functions.php' );
-define( 'USP_PROCESS_FORM_IF_ANY_PATH', get_stylesheet_directory() . '/user-submitted-posts/process-form-if-any.php' );
-define( 'USP_INIT_PATH', get_stylesheet_directory() . '/user-submitted-posts/init.php' );
+define( 'USP_CORE_CONSTANTS_AND_FUNCTIONS_PATH', get_stylesheet_directory() . '/user-submitted-posts/definitions/core-constants-and-functions.php' );
+define( 'USP_FORM_DISPLAYING_FUNCTIONS_PATH', get_stylesheet_directory() . '/user-submitted-posts/definitions/form-displaying-functions.php' );
+define( 'USP_FORM_PROCESSING_FUNCTIONS_PATH', get_stylesheet_directory() . '/user-submitted-posts/definitions/form-processing-functions.php' );
+define( 'USP_PROCESS_FORM_IF_ANY_PATH', get_stylesheet_directory() . '/user-submitted-posts/scripts/process-form-if-any.php' );
+define( 'USP_INIT_PATH', get_stylesheet_directory() . '/user-submitted-posts/scripts/init.php' );
 
 require_once( USP_INIT_PATH );
 require_once( USP_PROCESS_FORM_IF_ANY_PATH );
