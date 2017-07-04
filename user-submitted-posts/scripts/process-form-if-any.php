a535c339125190551b0cda76257bbae1713edb03
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-07-04 19:01:01 +0200

    Delete UPS

--- user-submitted-posts/scripts/process-form-if-any.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if ( ! defined( 'USP_INIT_FILE_REQUIRED' ) ) {
-	die( "USP init file must be included before any other file." );
-}
-
-require_once( USP_REQUEST_PROCESSING_FUNCTIONS_PATH );
-
-add_action( 'parse_request', 'usp_process_request', 1 );
