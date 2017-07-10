440047286327f499d9bd05d24d9ad71c4cab0b04
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-07-10 17:18:14 +0200

    Prevent archives from being stripped off their posts

--- functions/restrict-query-to-user-language.php
+++ functions/restrict-query-to-user-language.php
@@ -1,6 +1,10 @@
 <?php
 
 function restrict_query_to_user_language( $query ) {
+	// Checks if the query is only for a category
+	if ( 1 === sizeof( $query->query ) && isset( $query->query['category_name'] ) ) {
+		return;
+	}
 	$l10n_excluded_category = get_category_by_slug( APDD_L10N_EXCLUDED_CATEGORIES );
 	if ( false !== $l10n_excluded_category ) {
 		$l10n_excluded_category_id = $l10n_excluded_category->term_id;
