9698205474c2fbea6ed30e980a05dfa9d1525f74
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-07-10 15:37:10 +0200

    Add runtime translations for the Neptune Theme

--- languages/fr_FR.php
+++ languages/fr_FR.php
@@ -1,5 +1,429 @@
 <?php
 
 $current_language = array(
-	'Make sure the query is spelled correctly and try again:' => 'Assurez-vous de ne pas avoir fait de fautes de frappe et réessayez.',
+	'Make sure the query is spelled correctly and try again:'
+		=> 'Assurez-vous de ne pas avoir fait de fautes de frappe et réessayez.',
+
+	'No results found'
+		=> 'Aucun résultat',
+	'Make sure the url is spelled correctly and try again:'
+		=> 'Vérifiez que l\'URL soit écrite correctement et réessayez : ',
+	'All posts by %s'
+		=> 'Tous les posts par %s',
+	'My Recipes'
+		=> 'Mes Recettes',
+	'Status: '
+		=> 'Statut : ',
+	'Edit Recipe'
+		=> 'Éditer la Recette',
+	'One Comment'
+		=> 'Un Commentaire',
+	'%1$s Comments'
+		=> '%1$s Commentaires',
+	'Hide Comments'
+		=> 'Masquer les Commentaires',
+	'Comment navigation'
+		=> 'Navigation des commentaires',
+	'Older Comments'
+		=> 'Commentaires Moins Récents',
+	'Newer Comments'
+		=> 'Commentaires Plus Récents',
+	'Comments are closed.'
+		=> 'Les commentaires sont fermés.',
+	'Add Your Comment'
+		=> 'Ajouter Votre Commentaire',
+	'Make sure the query is spelled correctly and try again:'
+		=> 'Assurez-vous que la requête soit correcte et réessayez.',
+	'Share'
+		=> 'Partager',
+	'GIF'
+		=> 'GIF',
+	'Cooking Time: '
+		=> 'Temps de Cuisson',
+	'Read More'
+		=> 'Lire Plus',
+	'Comment'
+		=> 'Commentaire',
+	'Comments'
+		=> 'Commentaires',
+	'Views'
+		=> 'Vues',
+	'Top Menu'
+		=> 'Menu de l\'en-tête',
+	'Footer Menu'
+		=> 'Menu du Pied de Page',
+	'Archives/Index Sidebar'
+		=> 'Barre Latérale de Archives/Index',
+	'Footer Sidebar'
+		=> 'Barre Latérale du Pied de Page',
+	'Single Recipe Sidebar'
+		=> 'Barre Latérale des Recettes Seules',
+	'Single Recipe Comments Area Sidebar'
+		=> 'Barre Latérale de la Zone des Commentaires des Recettes Seules',
+	'Google font: on or off'
+		=> 'Police Google : activée ou non',
+	'Logged in as'
+		=> 'Connecté en tant que : ',
+	'My Profile'
+		=> 'Mon Profile',
+	'My Bookmarks'
+		=> 'Mes Marque-pages',
+	'Logout'
+		=> 'Déconnexion',
+	'Login'
+		=> 'Connexion',
+	'Register'
+		=> 'Devenir Ambassadeur',
+	'View your shopping cart'
+		=> 'Voir votre panier',
+	'%d item'
+		=> '%d objet',
+	'%d items'
+		=> '%d objets',
+	'Cart is empty'
+		=> 'Le panier est vide',
+	'Checkout Now'
+		=> 'Passer à la Caisse Maintenant',
+	'Checkout'
+		=> 'Passer à la Caisse',
+	'Install Required Plugins'
+		=> 'Installer les Extensions Requises',
+	'Install Plugins'
+		=> 'Installer les Extensions',
+	'Installing Plugin: %s'
+		=> 'Installation de l\'Extension : %s',
+	'Updating Plugin: %s'
+		=> 'Mise à Jour de l\'Extension : %s',
+	'Something went wrong with the plugin API.'
+		=> 'L\'API de l\'extension a rencontré un problème.',
+	'This theme requires the following plugin: %1$s.'
+		=> 'Le thème nécessite l\'installation de l\'extension suivante : %1$s.',
+	'This theme requires the following plugins: %1$s.'
+		=> 'Le thème nécessite l\'installation des extensions suivantes : %1$s.',
+	'This theme recommends the following plugin: %1$s.'
+		=> 'Le thème recommande l\'extension suivante : %1$s.',
+	'This theme recommends the following plugins: %1$s.'
+		=> 'Le thèmee recommande les extensions suivantes : %1$s.',
+	'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.'
+		=> 'Les extensions suivantes ont besoin d\'être mises à jour pour une compatibilité maximale avec ce thème : %1$s.',
+	'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.'
+		=> 'Les extensions suivantes ont besoin d\'être mises à jour pour une compatibilité maximale avec ce thème : %1$s.',
+	'There is an update available for: %1$s.'
+		=> 'Il y a une mise à jour disponible pour : %1$s.',
+	'There are updates available for the following plugins: %1$s.'
+		=> 'Il y a des mises à jour disponibles pour les extensions suivantes : %1$s.',
+	'The following required plugin is currently inactive: %1$s.'
+		=> 'L\'extension suivante, qui est requise par le thème, est actuellement inactive : %1$s.',
+	'The following required plugins are currently inactive: %1$s.'
+		=> 'Les extensions suivantes, qui sont requises par le thème, sont actuellement inactives : %1$s.',
+	'The following recommended plugin is currently inactive: %1$s.'
+		=> 'L\'extension suivante, qui est recommandée par le thème, est actuellement inactive : %1$s.',
+	'The following recommended plugins are currently inactive: %1$s.'
+		=> 'Les extensions suivantes, qui sont recommandées par le thème, sont actuellement inactives : %1$s.',
+	'Begin installing plugin'
+		=> 'Commencer l\'installation de l\'extension',
+	'Begin installing plugins'
+		=> 'Commencer l\'installation des extensions',
+	'Begin updating plugin'
+		=> 'Commencer la mise à jour de l\'extension',
+	'Begin updating plugins'
+		=> 'Commencer la mise à jour des extensions',
+	'Begin activating plugins'
+		=> 'Commencer l\'activation des extensions',
+	'Return to Required Plugins Installer'
+		=> 'Revenir vers l\'Installeur des Extensions Requises',
+	'Return to the Dashboard'
+		=> 'Revenir vers le Tableau de bord',
+	'Plugin activated successfully.'
+		=> 'L\'extension a été activée avec succès',
+	'The following plugin was activated successfully:'
+		=> 'L\'extension suivante a été activée avec succès : ',
+	'The following plugins were activated successfully:'
+		=> 'Les extensions suivantes ont été activées avec succès : ',
+	'No action taken. Plugin %1$s was already active.'
+		=> 'Rien n\'a été fait. L\extension %1$s était déjà active.',
+	'Plugin not activated. A higher version of %s is needed for this theme. '
+		=> 'L\'extension n\'a pas été activée. Une version supérieure à %s est requise pour ce thème.',
+	'Please update the plugin.'
+		=> 'Merci de mettre à jour l\'extension.',
+	'All plugins installed and activated successfully. %1$s'
+		=> 'Toutes les extensions installées ont été activées avec succès. %1$s',
+	'Dismiss this notice'
+		=> 'Masquer ce message',
+	'There are one or more required or recommended plugins to install, update or activate.'
+		=> 'Il y a au moins une extension requise ou recommandée à installer, mettre à jour ou activer.',
+	'Please contact the administrator of this site for help.'
+		=> 'Merci de contacter l\'administrateur de ce site pour obtenir de l\'aide.',
+	'This plugin needs to be updated to be compatible with your theme.'
+		=> 'L\'extension a besoin d\'être mise à jour pour être compatible avec votre thème.',
+	'Update Required'
+		=> 'Mise à Jour Nécessaire',
+	'The remote plugin package does not contain a folder with the desired slug and renaming did not work.'
+		=> 'Le progiciel d\'extensions distant ne contient pas un dossier avec l\'identifiant souhaité et ce dossier n\' pas pu être renommé.',
+	'Please contact the plugin provider and ask them to package their plugin according to the WordPress guidelines.'
+		=> 'Merci de contacter le fournisseur de l\'extension et de leur demander d\'assembler leur extension en suivant les recommendations WordPress.',
+	'The remote plugin package consists of more than one file, but the files are not packaged in a folder.'
+		=> 'Le progiciel de l\'extension distant est constitué de plusieurs fichiers, mais ces fichiers ne sont pas rangés dans un dossier.',
+	'and'
+		=> 'et',
+	'TGMPA v%s'
+		=> 'TGMPA v%s',
+	'Required'
+		=> 'Requis',
+	'Recommended'
+		=> 'Recommendé',
+	'WordPress Repository'
+		=> 'Répertoire WordPress',
+	'External Source'
+		=> 'Source Externe',
+	'Pre-Packaged'
+		=> 'Pré-Assemblé',
+	'Not Installed'
+		=> 'Non Installé',
+	'Installed But Not Activated'
+		=> 'Installé Mais Non Activé',
+	'Active'
+		=> 'Actif/Active',
+	'Required Update not Available'
+		=> 'Mise à Jour Requise non Disponible',
+	'Requires Update'
+		=> 'Nécessite une Mise à Jour',
+	'Update recommended'
+		=> 'Mise à jour recommandée',
+	'%1$s, %2$s'
+		=> '%1$s, %2$s',
+	'All <span class="count">(%s)</span>'
+		=> 'Tous <span class="count">(%s)</span>',
+	'To Install <span class="count">(%s)</span>'
+		=> 'Pour Installer <span class="count">(%s)</span>',
+	'Update Available <span class="count">(%s)</span>'
+		=> 'Mise à Jour Disponible <span class="count">(%s)</span>',
+	'To Activate <span class="count">(%s)</span>'
+		=> 'Pour Activer <span class="count">(%s)</span>',
+	'unknown'
+		=> 'inconnu',
+	'Installed version:'
+		=> 'Version installée : ',
+	'Minimum required version:'
+		=> 'Version minimum requise : ',
+	'Available version:'
+		=> 'Version disponible : ',
+	'No plugins to install, update or activate.'
+		=> 'Aucune extension à installer, mettre à jour ou activer : ',
+	'Plugin'
+		=> 'Extension',
+	'Source'
+		=> 'Source',
+	'Type'
+		=> 'Type',
+	'Version'
+		=> 'Version',
+	'Status'
+		=> 'Statut',
+	'Install %2$s'
+		=> 'Installer %2$s',
+	'Update %2$s'
+		=> 'Mettre à Jour %2$s',
+	'Activate %2$s'
+		=> 'Activer %2$s',
+	'Upgrade message from the plugin author:'
+		=> 'Information sur la mise à niveau de l\'auteur de l\'extension : ',
+	'Install'
+		=> 'Installer',
+	'Update'
+		=> 'Mettre à Jour',
+	'Activate'
+		=> 'Activer',
+	'No plugins were selected to be installed. No action taken.'
+		=> 'Aucunes extensions n\'étaient sélectionnées pour être installées. Rien n\' été fait.',
+	'No plugins were selected to be updated. No action taken.'
+		=> 'Aucunes extensions n\'étaient sélectionnées pour être mises à jour. Rien n\' été fait.',
+	'No plugins are available to be installed at this time.'
+		=> 'Aucunes extensions ne peuvent être installées actuellement.',
+	'No plugins are available to be updated at this time.'
+		=> 'Aucunes extensions ne peuvent être mises à jour actuellement.',
+	'No plugins were selected to be activated. No action taken.'
+		=> 'Aucunes extensions n\'étaient sélectionnées pour être activées. Rien n\'a été fait.',
+	'No plugins are available to be activated at this time.'
+		=> 'Aucunes extensions ne peuvent être activées actuellement.',
+	'Plugin activation failed.'
+		=> 'L\extension n\'a pas pu être activé.',
+	'Updating Plugin %1$s (%2$d/%3$d)'
+		=> 'Mise à Jour de l\'Extension %1$s (%2$d/%3$d)',
+	'An error occurred while installing %1$s: <strong>%2$s</strong>.'
+		=> 'L\'installation a rencontré un problème : <strong>%2$s</strong>.',
+	'The installation of %1$s failed.'
+		=> '%1$s n\'a pas pu être installée.',
+	'The installation and activation process is starting. This process may take a while on some hosts, so please be patient.'
+		=> 'Les processus d\'installation et d\'activation ont débuté. Cela peut prendre un certain temps.',
+	'%1$s installed and activated successfully.'
+		=> '%1$s installé et activé avec succès.',
+	'Show Details'
+		=> 'Afficher les Détails',
+	'Hide Details'
+		=> 'Masquer les Détails',
+	'All installations and activations have been completed.'
+		=> 'Toutes les installations et activations sont finies.',
+	'Installing and Activating Plugin %1$s (%2$d/%3$d)'
+		=> 'Installation et Activation de l\'Extension %2$s (%2$d/%3$d)',
+	'The installation process is starting. This process may take a while on some hosts, so please be patient.'
+		=> 'L\'installation a commencé. Cela peut prendre un certain temps.',
+	'%1$s installed successfully.'
+		=> '%1$s installée avec succès.',
+	'All installations have been completed.'
+		=> 'Toutes les installations sont finies.',
+	'Installing Plugin %1$s (%2$d/%3$d)'
+		=> 'Installation de l\'Extension %1$s (%2$d/%3$d)',
+	'No recipes found'
+		=> 'Aucune recette trouvée',
+	'Invalid data supplied'
+		=> 'Données reçues invalides',
+	'Liked'
+		=> 'Aimé',
+	'Like'
+		=> 'J\'aime',
+	'Loading…'
+		=> 'Chargement…',
+	'Vote'
+		=> 'Vote',
+	'%d Servings'
+		=> '%d Portions',
+	'Cooking Time:'
+		=> 'Temps de Cuisson : ',
+	'Easy'
+		=> 'Facile',
+	'Medium'
+		=> 'Intermédiaire',
+	'Hard'
+		=> 'Difficile',
+	'Previous Entries'
+		=> 'Précédent',
+	'Next Entries'
+		=> 'Suivant',
+	'Home'
+		=> 'Accueil',
+	'View all posts in %s'
+		=> 'Voir tout dans %s',
+	'failed to create css file'
+		=> 'échoua à créer le fichier CSS',
+	'Recipes'
+		=> 'Recettes',
+	'Search by ingredients'
+		=> 'Recherche par ingrédient',
+	'Click to select Ingredients for search...'
+		=> 'Cliquez pour sélectionner des Ingrédients à rechercher…',
+	'Searching...'
+		=> 'Recherche…',
+	'Find Recipes'
+		=> 'Explorer les Recettes',
+	'Submit Recipe'
+		=> 'Proposer une Recette',
+	'Your recipe has been submitted. We will review it and publish it shortly. In the meantime you can either publish another one using a form below or just continue browsing our website.'
+		=> 'Votre recette a été proposée. Nous allons la vérifier et la publier rapidement. Vous pouvez tout à fait en proposer une autre en attendant.',
+	'Update Recipe'
+		=> 'Mettre à Jour la Recette',
+	'Your recipe has been updated. We will review it and publish it shortly. In the meantime you can either submit/edit another one using a form below or just continue browsing our website.'
+		=> 'Votre recette a été mise à jour. Nous allons la vérifier et la publier rapidement. Vous pouvez tout à fait en mettre à jour une autre.',
+	'Submit your recipe'
+		=> 'Soumettre votre recette',
+	'Edit your recipe'
+		=> 'Mettre à jour votre recette',
+	'You do not have required permissions to submit recipes'
+		=> 'Vous n\'avez pas les autorisations nécessaires pour proposer des recettes',
+	'Login to submit your recipe'
+		=> 'Connectez-vous pour proposer votre recette',
+	'You do not have required permissions to edit this recipe.'
+		=> 'Vous n\'avez pas les autorisations nécessaires pour mettre à jour cette recette.',
+	'Recent Posts by %s:'
+		=> 'Articles Récents par %s : ',
+	'"%s" <span class="smaller-text">Query Search Results</span>'
+		=> '"%s" <span class="smaller-text">Résultats de la Recherche</span>',
+	'Search …'
+		=> 'Recherche…',
+	'Search for:'
+		=> 'Rechercher : ',
+	'Search'
+		=> 'Recherche',
+	'Start Reading Mode'
+		=> 'Commencer le Mode Lecture',
+	'Print'
+		=> 'Imprimer',
+	'Resume'
+		=> 'Continuer',
+	'Pause'
+		=> 'Pause',
+	'min'
+		=> 'min',
+	'Stop'
+		=> 'Stop',
+	'Start Timer'
+		=> 'Lancer le Minuteur',
+	'Cooking Time: %s'
+		=> 'Temps de Cuisson : %s',
+	'Serves %d'
+		=> 'Pour %d',
+	'Recipe Difficulty: %s'
+		=> 'Difficulté de la Recette : %s',
+	'Cooking Temperature: %s'
+		=> 'Température de Cuisson : %s',
+	'Share it on your social network:'
+		=> 'Partager sur les réseaux sociaux : ',
+	'Or you can just copy and share this url'
+		=> 'Ou vous pouvez simplement copier et partager cette url',
+	'Ingredients'
+		=> 'Ingrédients',
+	'Adjust Servings:'
+		=> 'Régler le Nombre de Portions : ',
+	'Nutritional information'
+		=> 'Informations Nutritionelles',
+	'Bookmark this recipe'
+		=> 'Ajouter cette recette aux marque-pages',
+	'Features:'
+		=> 'Caractéristiques : ',
+	'Cuisine:'
+		=> 'Cuisine : ',
+	'Directions'
+		=> 'Instructions',
+	'Steps'
+		=> 'Étapes',
+	'Done'
+		=> 'Fait',
+	'Recipe Reviews'
+		=> 'Évaluations de la Recette',
+	'Average Rating: '
+		=> 'Score Moyen : ',
+	'Total Reviews: '
+		=> 'Nombre Total d\Évaluations : ',
+	'There are no reviews for this recipe yet, use a form below to write your review'
+		=> 'Il n\y a aucune évaluation pour cette recette pour l\'instant, utilisez ce formulaire pour écrire la votre',
+	'Thank you for submitting your review, you can find it in a list above as soon as we process it.'
+		=> 'Merci d\'avoir proposé votre évaluation, vous pourrez la trouver dans la liste ci-dessus dès que nous l\'avons traitée',
+	'Write your own review'
+		=> 'Écrivez votre propre évaluation',
+	'Submit Review'
+		=> 'Soumettre votre Évaluation',
+	'Login to write a review'
+		=> 'Connectez-vous pour écrire une évaluation',
+	'previous'
+		=> 'précédent',
+	'next'
+		=> 'suivant',
+	'Related Recipes:'
+		=> 'Recettes Suggérés : ',
+	'Posted on'
+		=> 'Posté le',
+	'by'
+		=> 'par',
+	'Categories:'
+		=> 'Catégories : ',
+	'Neptune by Osetin'
+		=> 'Neptune par Osetin',
+	'http://neptune.pinsupreme.com'
+		=> 'http://neptune.pinsupreme.com',
+	'Food Recipes Wordpress Theme'
+		=> 'Thème WordPress Food Recipes',
+	'Tamik Soziev'
+		=> 'Tamik Soziev',
+	'http://soziev.com/'
+		=> 'http://soziev.com/',
 );
