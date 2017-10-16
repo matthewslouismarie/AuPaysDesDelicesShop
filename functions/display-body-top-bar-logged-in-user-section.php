5ab61d9b3f4511644855884a7c76ba6e0dbb9bf3
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-10-16 18:39:18 +0100

    Give .item and .link classes to userpro li and a in navbar

--- functions/display-body-top-bar-logged-in-user-section.php
+++ functions/display-body-top-bar-logged-in-user-section.php
@@ -11,22 +11,22 @@ function display_body_top_bar_logged_in_user_section() {
 	
 	?>
 
-	<li>
+	<li class="item">
 		<span>
 			<?= __("Logged in as", 'osetin'); ?> <strong><?= $os_current_user->display_name; ?></strong>
 		</span>
 	</li>
 
-	<li>
-		<a href="<?= $userpro->permalink( get_current_user_id() ); ?>">
+	<li class="item">
+		<a class="link" href="<?= $userpro->permalink( get_current_user_id() ); ?>">
 			<i class="os-icon os-icon-head"></i>
 			<span><?php _e('My Profile', 'osetin') ?></span>
 		</a>
 	</li>
 
 	<?php if ( user_has_posts( get_current_user_id() ) ) : ?>
-	<li>
-		<a href="<?= get_author_posts_url( get_current_user_id() ); ?>">
+	<li class="item">
+		<a class="link" href="<?= get_author_posts_url( get_current_user_id() ); ?>">
 			<i class="os-icon os-icon-home-03"></i>
 			<span><?= __( 'My Recipes' ) ?></span>
 		</a>
@@ -34,8 +34,8 @@ function display_body_top_bar_logged_in_user_section() {
 	<?php endif; ?>
 
 	<?php if ( page_exists( BOOKMARKS_SLUG ) ) : ?>
-	<li>
-		<a href="<?= get_page_url_by_slug( BOOKMARKS_SLUG ) ?>">
+	<li class="item">
+		<a class="link" href="<?= get_page_url_by_slug( BOOKMARKS_SLUG ) ?>">
 			<i class="os-icon os-icon-ui-33"></i>
 			<span><?= get_page_title_from_slug( BOOKMARKS_SLUG ) ?></span>
 		</a>
@@ -43,24 +43,24 @@ function display_body_top_bar_logged_in_user_section() {
 	<?php endif; ?>
 
 	<?php if ( page_exists( COMMENTS_SLUG ) ) : ?>
-	<li>
-		<a href="<?= get_page_url_by_slug( COMMENTS_SLUG ) ?>">
+	<li class="item">
+		<a class="link" href="<?= get_page_url_by_slug( COMMENTS_SLUG ) ?>">
 			<span><?= get_page_title_from_slug( COMMENTS_SLUG ) ?></span>
 		</a>
 	</li>
 	<?php endif; ?>
 
 	<?php if ( page_exists( MEAL_PLANS_SLUG ) ) : ?>
-	<li>
-		<a href="<?= get_page_url_by_slug( MEAL_PLANS_SLUG ) ?>">
+	<li class="item">
+		<a class="link" href="<?= get_page_url_by_slug( MEAL_PLANS_SLUG ) ?>">
 			<i class="os-icon os-icon-ui-33"></i>
 			<span><?= get_page_title_from_slug( MEAL_PLANS_SLUG ) ?></span>
 		</a>
 	</li>
 	<?php endif; ?>
 
-	<li>
-		<a href="<?= wp_logout_url(); ?>">
+	<li class="item">
+		<a class="link" href="<?= wp_logout_url(); ?>">
 			<i class="os-icon os-icon-signs-12"></i>
 			<span>
 				<?php _e('Logout', 'osetin') ?>
--- functions/display-body-top-bar-visitor-section.php
+++ functions/display-body-top-bar-visitor-section.php
@@ -3,14 +3,14 @@
 function display_body_top_bar_visitor_section() {
 ?>
 
-<li>
-	<a href="#" class="popup-login">
+<li class="item">
+	<a class="link" href="#" class="popup-login">
 		<?php _e('Login', 'osetin'); ?>
 	</a>
 </li>
 
-<li>
-	<a href="<?= get_page_url_by_slug( 'devenir-ambassadeur' ) ?>">
+<li class="item">
+	<a class="link" href="<?= get_page_url_by_slug( 'devenir-ambassadeur' ) ?>">
 		<?= __( 'Join the community', APDD_TEXT_DOMAIN ) ?>
 	</a>
 </li>
